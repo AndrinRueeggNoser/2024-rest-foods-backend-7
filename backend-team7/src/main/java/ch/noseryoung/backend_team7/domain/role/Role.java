ef9ed1b04c14a429f7a97554f3e53a17391d2888
MaximilianNoethe <maximilian.noethe@noseryoung.com>
2024-06-13 09:28:24 +0200

    Create all fields in role

--- backend-team7/src/main/java/ch/noseryoung/backend_team7/domain/role/Role.java
+++ backend-team7/src/main/java/ch/noseryoung/backend_team7/domain/role/Role.java
@@ -1,4 +1,20 @@
 package ch.noseryoung.backend_team7.domain.role;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import lombok.Getter;
+import lombok.Setter;
 
+@Entity
+@Getter
+@Setter
+@Table(name = "role")
 public class Role {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "role_id")
+    private int roleId;
+
+    @Column(name = "role_name")
+    private String roleName;
+
 }
