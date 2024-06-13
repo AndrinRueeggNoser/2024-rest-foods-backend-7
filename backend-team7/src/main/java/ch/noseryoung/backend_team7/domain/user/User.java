b76e8e3e16dcfd8ebc4da88a1f35e53e0ae8622c
MaximilianNoethe <maximilian.noethe@noseryoung.com>
2024-06-13 10:27:08 +0200

    Format User class

--- backend-team7/src/main/java/ch/noseryoung/backend_team7/domain/user/User.java
+++ backend-team7/src/main/java/ch/noseryoung/backend_team7/domain/user/User.java
@@ -1,4 +1,29 @@
 package ch.noseryoung.backend_team7.domain.user;
 
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@Table(name = "user")
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "user_id")
+    private int userId;
+
+    @Column(name = "first_name")
+    private String firstName;
+
+    @Column(name = "last_name")
+    private String lastName;
+
+    @Column(name = "phone_number")
+    private String phoneNumber;
+
+    @Column(name = "password")
+    private String password;
+
 }
