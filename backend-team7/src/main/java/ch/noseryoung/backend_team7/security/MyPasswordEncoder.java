13dd97ad5561a93acef0e4be7da27b8760ed851a
MaximilianNoethe <maximilian.noethe@noseryoung.com>
2024-06-13 15:35:07 +0200

    Delete unnecessary import

--- backend-team7/src/main/java/ch/noseryoung/backend_team7/security/MyPasswordEncoder.java
+++ backend-team7/src/main/java/ch/noseryoung/backend_team7/security/MyPasswordEncoder.java
@@ -2,7 +2,6 @@ package ch.noseryoung.backend_team7.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
