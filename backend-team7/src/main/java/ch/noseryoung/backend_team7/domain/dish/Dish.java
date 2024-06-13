6666748de47cce6b0571f4a56516da2a79a233e2
aryanbisen <aryan.bisen@noseryoung.com>
2024-06-13 09:00:42 +0200

    Rename fields

--- backend-team7/src/main/java/ch/noseryoung/backend_team7/domain/dish/Dish.java
+++ backend-team7/src/main/java/ch/noseryoung/backend_team7/domain/dish/Dish.java
@@ -1,26 +1,28 @@
 package ch.noseryoung.backend_team7.domain.dish;
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 import lombok.Setter;
 
 @Entity
 @Getter
 @Setter
-@Table(name = "Dish")
+@Table(name = "dish")
 public class Dish {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id")
-    private int id;
+    private int dishId;
 
-    @Column(name = "name")
+    @Column(name = "dishName")
+    @NotNull
     private String name;
 
     @Column(name = "description")
     private String description;
 
-    @Column(name = "category")
-    private String category;
+    @Column(name = "region")
+    private String region;
 
     @Column(name = "price")
     private Double price;
