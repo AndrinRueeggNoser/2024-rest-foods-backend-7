03369c90ba5db6b67b83261fb56f41378e25ddc6
KennethDang1 <kenneth.dang@noseryoung.com>
2024-06-13 14:16:46 +0200

    Finish insert statements in data.sql

--- backend-team7/src/main/resources/data.sql
+++ backend-team7/src/main/resources/data.sql
@@ -1,2 +1,37 @@
-INSERT INTO dish ( dish_name, description, image, is_chefs_choice, region, price)
-VALUES ('Pizza', 'Italian speciality', 'https://www.pizza.png', FALSE, 'Italy', 12);
+-- DISH
+INSERT INTO dish (dish_name, description, image, is_chefs_choice, region, price)
+VALUES ('Pizza', 'Italian speciality', 'https://www.pizza.png', FALSE, 'Italy', 12),
+       ('Sushi', 'Japanese speciality', 'https://www.sushi.png', TRUE, 'Japan', 20.00),
+       ('Taco', 'Mexican speciality', 'https://www.taco.png', FALSE, 'Mexico', 8.50);
+
+-- RESERVATION
+INSERT INTO reservation (table_no, table_is_available, start_time, end_time, person_count)
+VALUES (1, TRUE, 1800, 2000, 4),
+       (2, FALSE, 1900, 2100, 2),
+       (3, TRUE, 2000, 2200, 6);
+
+-- ROLES
+INSERT INTO role(role_id, role_name)
+VALUES (1, 'ADMIN'),
+       (2, 'USER');
+
+
+-- AUTHORITIES
+INSERT INTO authority (authority_id, authority_name)
+VALUES (1, 'GET'),
+       (2, 'POST'),
+       (3, 'PUT'),
+       (4, 'DELETE');
+
+-- ADMIN can CRUD, User can R
+INSERT INTO role_authorities (id_authority, id_role)
+VALUES (1, 1),
+       (2, 1),
+       (3, 1),
+       (4, 1),
+       (1, 2);
+
+-- USER
+INSERT INTO users (first_name, last_name, phone_number, password, id_role)
+VALUES ('ADMIN', 'ADMIN', '1234567890', '1234', 1),
+       ('USER', 'USER', '0987654321', '1234', 2);
