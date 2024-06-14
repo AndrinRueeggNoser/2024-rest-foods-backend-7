22c841e126075298361acde98cccf2fcc2f0dd5f
MaximilianNoethe <maximilian.noethe@noseryoung.com>
2024-06-14 10:34:09 +0200

    Implement CRUD operations in ReservationService
    Couple possibilities that could occur, still need to be checked. These checks can only be implemented when table entity is created.

--- backend-team7/src/main/java/ch/noseryoung/backend_team7/domain/reservation/ReservationService.java
+++ backend-team7/src/main/java/ch/noseryoung/backend_team7/domain/reservation/ReservationService.java
@@ -1,4 +1,52 @@
 package ch.noseryoung.backend_team7.domain.reservation;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import javax.management.InstanceAlreadyExistsException;
+import javax.management.InstanceNotFoundException;
+import java.util.List;
+
+@Service
 public class ReservationService {
+    @Autowired
+    private ReservationRepository reservationRepository;
+
+    public List<Reservation> getAllReservations() {
+        return reservationRepository.findAll();
+    }
+
+    public Reservation getByID(int reservationId) throws InstanceNotFoundException {
+        return reservationRepository.findById(reservationId).orElseThrow(() -> new InstanceNotFoundException("Reservation with id " + reservationId + " could not be found."));
+    }
+    /**
+     * When selected table is already reserved, exception should be thrown.
+     * If restaurant is full (no reservations available), exception should be thrown.
+     * If reservation time is too early or too late, exception should be thrown.
+     **/
+    public Reservation addReservation(Reservation newReservation) {
+        return reservationRepository.save(newReservation);
+    }
+
+    public Reservation updateReservation(Reservation reservation, int reservationId) throws InstanceNotFoundException, InstanceAlreadyExistsException {
+        if (!reservationRepository.existsById(reservationId)) {
+            throw new InstanceNotFoundException("Reservation with id " + reservationId + " could not be found.");
+        }
+        /**
+         * IF UPDATED RESERVATION:
+         * When selected table is already reserved, exception should be thrown.
+         * If restaurant is full (no reservations available), exception should be thrown.
+         * If reservation time is too early or too late, exception should be thrown.
+         * */
+        reservation.setReservationId(reservationId);
+        return reservationRepository.save(reservation);
+    }
+
+    public void deleteReservation(int reservationId) throws InstanceNotFoundException {
+        if (!reservationRepository.existsById(reservationId)) {
+            throw new InstanceNotFoundException("Reservation with id " + reservationId + " could not be found.");
+        }
+        reservationRepository.deleteById(reservationId);
+    }
+
 }
