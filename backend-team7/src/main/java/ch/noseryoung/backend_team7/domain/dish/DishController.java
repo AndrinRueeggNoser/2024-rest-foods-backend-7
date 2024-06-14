49ca528f9dc9e873c77edeaa737851b6f34f46d5
KennethDang1 <kenneth.dang@noseryoung.com>
2024-06-14 09:25:19 +0200

    Add getById method with Exceptionhandler

--- backend-team7/src/main/java/ch/noseryoung/backend_team7/domain/dish/DishController.java
+++ backend-team7/src/main/java/ch/noseryoung/backend_team7/domain/dish/DishController.java
@@ -1,36 +1,45 @@
 package ch.noseryoung.backend_team7.domain.dish;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.security.access.prepost.PreAuthorize;
 
 import java.util.List;
+import java.util.NoSuchElementException;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/dish")
 public class DishController {
-    @Autowired DishService dishService;
+    @Autowired
+    DishService dishService;
 
     // Implement Swagger! @Operation
     @GetMapping
-    public List<Dish> getAllDishes(){
+    public List<Dish> getAllDishes() {
         return dishService.getAllDishes();
+    }
+
+    @GetMapping("/{Id}")
+    public ResponseEntity<Dish> getById(@PathVariable("Id") int id) {
+        return ResponseEntity.ok().body(dishService.getById(id));
     }
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
-    public Dish createDish(@RequestBody Dish newDish){
+    public Dish createDish(@RequestBody Dish newDish) {
         return dishService.addDish(newDish);
     }
 
     @PutMapping(value = "/{dishId}")
-    public Dish updateDish(@PathVariable("dishId") int dishId, @RequestBody Dish dish){
+    public Dish updateDish(@PathVariable("dishId") int dishId, @RequestBody Dish dish) {
         return dishService.updateById(dishId, dish);
     }
 
-
-
-
+    @ExceptionHandler(NoSuchElementException.class)
+    public ResponseEntity<String> handleNoSuchElementException(NoSuchElementException nsee) {
+        return ResponseEntity.status(404).body(nsee.getMessage());
+    }
 
 }
--- backend-team7/src/main/java/ch/noseryoung/backend_team7/domain/dish/DishService.java
+++ backend-team7/src/main/java/ch/noseryoung/backend_team7/domain/dish/DishService.java
@@ -4,23 +4,32 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.NoSuchElementException;
 
 @Service
 public class DishService {
-    @Autowired DishRepository dishRepository;
-    public DishService(){
+    @Autowired
+    DishRepository dishRepository;
+
+    public DishService() {
 
     }
 
 
-    public List<Dish> getAllDishes(){
+    public List<Dish> getAllDishes() {
         return dishRepository.findAll();
     }
-    public Dish addDish(Dish newDish){
+
+    public Dish getById(int id) {
+        return dishRepository.findById(id).orElseThrow(() -> new NoSuchElementException("GET: Dish with id " + id + " could not be found."));
+    }
+
+
+    public Dish addDish(Dish newDish) {
         return dishRepository.save(newDish);
     }
 
-    public Dish updateById(int dishId, Dish dish){
+    public Dish updateById(int dishId, Dish dish) {
         dish.setDishId(dishId);
         return dishRepository.save(dish);
     }
