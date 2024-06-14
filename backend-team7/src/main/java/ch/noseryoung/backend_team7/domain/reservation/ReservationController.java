583f713be2e5e262fd6bf9993817d7b2875cff3f
MaximilianNoethe <maximilian.noethe@noseryoung.com>
2024-06-14 08:27:08 +0200

    Implement imports and annotations

--- backend-team7/src/main/java/ch/noseryoung/backend_team7/domain/reservation/ReservationController.java
+++ backend-team7/src/main/java/ch/noseryoung/backend_team7/domain/reservation/ReservationController.java
@@ -1,4 +1,13 @@
 package ch.noseryoung.backend_team7.domain.reservation;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
+@RestController
+@RequestMapping
 public class ReservationController {
 }
