67e621400e41fbd0c461a9d0a4ec38d50bee2b60
KennethDang1 <kenneth.dang@noseryoung.com>
2024-06-25 16:05:46 +0200

    Add validation to fields from reservation and dish

--- backend-team7/src/main/java/ch/noseryoung/backend_team7/domain/dish/Dish.java
+++ backend-team7/src/main/java/ch/noseryoung/backend_team7/domain/dish/Dish.java
@@ -1,6 +1,9 @@
 package ch.noseryoung.backend_team7.domain.dish;
 import jakarta.persistence.*;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -15,9 +18,12 @@ public class Dish {
     private int dishId;
 
     @Column(name = "dish_name")
+    @NotBlank(message = "Dish name cannot be blank")
+    @Size(max = 100, message = "Dish name must be less than 100 characters")
     private String dishName;
 
     @Column(name = "description")
+    @Size(max = 300, message = "Description must be less than 255 characters")
     private String description;
 
     @Column(name = "image")
@@ -27,9 +33,12 @@ public class Dish {
     private boolean isChefsChoice;
 
     @Column(name = "region")
+    @Size(max = 100, message = "Region must be less than 100 characters")
     private String region;
 
     @Column(name = "price")
+    @NotNull(message = "Price cannot be null")
+    @Min(value = 0, message = "Price must be a positive value")
     private Double price;
 
     public Dish(int dishId, String dishName, String description, String image, String region, Double price) {
--- backend-team7/src/main/java/ch/noseryoung/backend_team7/domain/reservation/Reservation.java
+++ backend-team7/src/main/java/ch/noseryoung/backend_team7/domain/reservation/Reservation.java
@@ -1,6 +1,9 @@
 package ch.noseryoung.backend_team7.domain.reservation;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.Future;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 import lombok.Setter;
 import ch.noseryoung.backend_team7.domain.table.RestaurantTable;
@@ -18,12 +21,17 @@ public class Reservation {
     private int reservationId;
 
     @Column(name = "reservation_time")
+    @NotNull(message = "Reservation time cannot be empty")
+    @Future(message = "Reservation time must be in the future")
     private LocalDateTime reservationTime;
 
     @Column(name = "person_count")
+    @Min(value = 1, message = "Person count must be at least 1")
     private int personCount;
 
     @OneToOne
     @JoinColumn(name = "id_table", referencedColumnName = "table_id")
+
+    @NotNull(message = "Restaurant table cannot be empty")
     private RestaurantTable restaurantTable;
 }
