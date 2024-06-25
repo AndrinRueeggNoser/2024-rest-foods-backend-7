06fc9eb0bf1217cd0043a86a47c966b1f2402a33
KennethDang1 <kenneth.dang@noseryoung.com>
2024-06-25 13:43:30 +0200

    Add Swagger documentation to reservation and dish controller

--- backend-team7/src/main/java/ch/noseryoung/backend_team7/domain/dish/DishController.java
+++ backend-team7/src/main/java/ch/noseryoung/backend_team7/domain/dish/DishController.java
@@ -1,37 +1,44 @@
 package ch.noseryoung.backend_team7.domain.dish;
+
+import io.swagger.v3.oas.annotations.Operation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
 import javax.management.InstanceAlreadyExistsException;
 import javax.management.InstanceNotFoundException;
 import java.util.List;
 import java.util.NoSuchElementException;
 import java.util.Optional;
+
 @RestController
 @RequestMapping("/dish")
 public class DishController {
     @Autowired
     DishService dishService;
 
-    // Implement Swagger! @Operation
-
-    /***
-     /**
+    /**
      * Gets all dishes
+     *
      * @return A list of all dishes
      */
+
     @GetMapping
+    @Operation(summary = "Get all dishes", description = "Displays a list of all dishes along with their id, name, description, image, region, price, chefs choice and returns a JSON with the status code 200.")
     public List<Dish> getAllDishes() {
         return dishService.getAllDishes();
     }
+
     /**
      * Gets a single dish by its id
+     *
      * @param dishId The id of the dish to get
      * @return Status code 200
      * @throws InstanceNotFoundException if the dish with the specified id is not found
      */
     @GetMapping("/{dishId}")
+    @Operation(summary = "Get dish by ID", description = "Retrieves a dishes information by its id and returns a JSON object with the status code 200.")
     public ResponseEntity<Dish> getById(@PathVariable("dishId") int dishId) throws InstanceNotFoundException {
         return ResponseEntity.ok().body(dishService.getById(dishId));
     }
@@ -44,6 +51,7 @@ public class DishController {
      * @throws InstanceAlreadyExistsException if a dish with the same id already exists
      */
     @PostMapping
+    @Operation(summary = "Create a new dish", description = "Adds a new dish with its information and returns a JSON object with the status code 201.")
     public ResponseEntity<Dish> createDish(@RequestBody Dish newDish) throws InstanceAlreadyExistsException {
         return ResponseEntity.status(201).body(dishService.addDish(newDish));
     }
@@ -58,6 +66,7 @@ public class DishController {
      * @throws InstanceAlreadyExistsException if a dish with the same id already exists
      */
     @PutMapping(value = "/{dishId}")
+    @Operation(summary = "Update a dish", description = "Updates the information of an existing dish and returns a JSON object with the status code 200.")
     public ResponseEntity<Dish> updateDish(@PathVariable("dishId") int dishId, @RequestBody Dish dish) throws InstanceNotFoundException, InstanceAlreadyExistsException {
         return ResponseEntity.status(200).body(dishService.updateById(dishId, dish));
     }
@@ -71,6 +80,7 @@ public class DishController {
      * @throws InstanceNotFoundException if the dish with the specified id is not found
      */
     @DeleteMapping("{dishId}")
+    @Operation(summary = "Delete a dish", description = "Removes a dish using its id and returns a message with the status code 200.")
     public String deleteDish(@PathVariable("dishId") int dishId) throws InstanceNotFoundException {
         dishService.deleteDish(dishId);
         return "Dish with id " + dishId + " was successfully deleted.";
--- backend-team7/src/main/java/ch/noseryoung/backend_team7/domain/reservation/ReservationController.java
+++ backend-team7/src/main/java/ch/noseryoung/backend_team7/domain/reservation/ReservationController.java
@@ -1,6 +1,7 @@
 package ch.noseryoung.backend_team7.domain.reservation;
 
 import ch.noseryoung.backend_team7.domain.dish.Dish;
+import io.swagger.v3.oas.annotations.Operation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -9,6 +10,7 @@ import org.springframework.web.bind.annotation.*;
 import javax.management.InstanceAlreadyExistsException;
 import javax.management.InstanceNotFoundException;
 import java.util.List;
+
 /**
  * TODO:
  * Once exceptions are added in ReservationService, they should also be added into
@@ -24,30 +26,36 @@ public class ReservationController {
 
     /**
      * Gets all reservations
+     *
      * @return A list of all reservations
      */
     @GetMapping
+    @Operation(summary = "Get all reservations", description = "Displays a list of all reservations along with their id, restaurant table, start time, end time, person count and returns a JSON with the status code 200.")
     public List<Reservation> getAll() {
         return reservationService.getAllReservations();
     }
 
     /**
      * Gets a single reservation by its id
+     *
      * @param reservationId The id of the reservation to get
      * @return Status code 200
      * @throws InstanceNotFoundException if the reservation with the specified id is not found
      */
     @GetMapping("/{reservationId}")
+    @Operation(summary = "Get reservation by ID", description = "Retrieves a reservations information by its id and returns a JSON object with the status code 200.")
     public ResponseEntity<Reservation> getById(@PathVariable("reservationId") int reservationId) throws InstanceNotFoundException {
         return ResponseEntity.ok().body(reservationService.getByID(reservationId));
     }
 
     /**
      * Creates a new reservation
+     *
      * @param newReservation The reservation object to create
      * @return Status code 201
      */
     @PostMapping
+    @Operation(summary = "Create a new reservation", description = "Adds a new reservation with its information and returns a JSON object with the status code 201.")
     @ResponseStatus(HttpStatus.CREATED)
     public ResponseEntity<Reservation> createReservation(@RequestBody Reservation newReservation) {
         return ResponseEntity.status(201).body(reservationService.addReservation(newReservation));
@@ -55,31 +63,36 @@ public class ReservationController {
 
     /**
      * Updates an existing reservation
+     *
      * @param reservationId The id of the reservation to update
-     * @param reservation Updated reservation data
+     * @param reservation   Updated reservation data
      * @return Status code 200
-     * @throws InstanceNotFoundException if the reservation with the specified id is not found
+     * @throws InstanceNotFoundException      if the reservation with the specified id is not found
      * @throws InstanceAlreadyExistsException if a reservation like this already exists
      */
     @PutMapping(value = "/{reservationId}")
+    @Operation(summary = "Update a reservation", description = "Updates the information of an existing reservation and returns a JSON object with the status code 200.")
     public ResponseEntity<Reservation> updateReservation(@PathVariable("reservationId") int reservationId, @RequestBody Reservation reservation) throws InstanceNotFoundException, InstanceAlreadyExistsException {
         return ResponseEntity.status(200).body(reservationService.updateReservation(reservation, reservationId));
     }
 
     /**
      * Deletes a reservation by its id
+     *
      * @param reservationId Used to delete a certain reservation
      * @return "Success" message for deleting a reservation
      * @throws InstanceNotFoundException if the reservation with the specified id is not found
      */
     @DeleteMapping("/{reservationId}")
-    public String deleteRank(@PathVariable("reservationId") int reservationId) throws InstanceNotFoundException {
+    @Operation(summary = "Delete a reservation", description = "Removes a reservation using its id and returns a message with the status code 200.")
+    public String deleteDish(@PathVariable("reservationId") int reservationId) throws InstanceNotFoundException {
         reservationService.deleteReservation(reservationId);
         return "Reservation with id " + reservationId + " was successfully deleted.";
     }
 
     /**
      * Handles InstanceNotFoundException
+     *
      * @param infe The exception to handle
      * @return Status code 404 and error message
      */
@@ -90,9 +103,11 @@ public class ReservationController {
 
     /**
      * Handles InstanceAlreadyExistsException
+     *
      * @param iaee The exception to handle
      * @return Status code 404 and error message
      */
+
     @ExceptionHandler(InstanceAlreadyExistsException.class)
     public ResponseEntity<String> instanceAlreadyExistException(InstanceAlreadyExistsException iaee) {
         return ResponseEntity.status(404).body(iaee.getMessage());
