1a4789c1d87749e5af4d4933315a7175a1361741
MaximilianNoethe <maximilian.noethe@noseryoung.com>
2024-06-25 15:55:31 +0200

    Update repository

--- backend-team7/src/main/java/ch/noseryoung/backend_team7/domain/table/RestaurantTableRepository.java
+++ backend-team7/src/main/java/ch/noseryoung/backend_team7/domain/table/RestaurantTableRepository.java
@@ -5,5 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface RestaurantTableRepository extends JpaRepository<Reservation, Integer> {
+public interface RestaurantTableRepository extends JpaRepository<RestaurantTable, Integer> {
 }
