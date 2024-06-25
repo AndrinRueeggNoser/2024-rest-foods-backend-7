42054a11d0e91db53c88f23d4ae6a14448adef6d
MaximilianNoethe <maximilian.noethe@noseryoung.com>
2024-06-25 08:47:32 +0200

    Implement javadoc in dish controller

--- backend-team7/src/main/java/ch/noseryoung/backend_team7/domain/dish/DishController.java
+++ backend-team7/src/main/java/ch/noseryoung/backend_team7/domain/dish/DishController.java
@@ -19,6 +19,10 @@ public class DishController {
 
     // Implement Swagger! @Operation
 
+    /***
+     * Gets all dishes
+     * @return A List of all dishes
+     */
     @GetMapping
     public List<Dish> getAllDishes() {
         return dishService.getAllDishes();
@@ -26,36 +30,66 @@ public class DishController {
 
     /**
      * Gets a single dish by its id
-     * @param dishId Get object to get a dish by id
+     *
+     * @param dishId The id of the dish to get
      * @return Status code 200
-     * @throws InstanceNotFoundException
+     * @throws InstanceNotFoundException if the dish with the specified id is not found
      */
     @GetMapping("/{dishId}")
     public ResponseEntity<Dish> getById(@PathVariable("dishId") int dishId) throws InstanceNotFoundException {
         return ResponseEntity.ok().body(dishService.getById(dishId));
     }
 
+    /***
+     * Creates a new dish
+     * @param newDish The dish object to create
+     * @return Status code 201
+     * @throws InstanceAlreadyExistsException if a dish with the same id already exists
+     */
     @PostMapping
     public ResponseEntity<Dish> createDish(@RequestBody Dish newDish) throws InstanceAlreadyExistsException {
         return ResponseEntity.status(201).body(dishService.addDish(newDish));
     }
 
+    /***
+     * Updates an existing dish
+     * @param dishId The id of the dish to update
+     * @param dish Updated dish data
+     * @return Status code 200
+     * @throws InstanceNotFoundException if the dish with the specified id is not found
+     */
     @PutMapping(value = "/{dishId}")
     public ResponseEntity<Dish> updateDish(@PathVariable("dishId") int dishId, @RequestBody Dish dish) throws InstanceNotFoundException {
         return ResponseEntity.status(200).body(dishService.updateById(dishId, dish));
     }
 
+    /***
+     * Deletes a dish by its id
+     * @param dishId Used to delete a certain dish
+     * @return "Success" message for deleting a dish
+     * @throws InstanceNotFoundException
+     */
     @DeleteMapping("{dishId}")
     public String deleteDish(@PathVariable("dishId") int dishId) throws InstanceNotFoundException {
         dishService.deleteDish(dishId);
         return "Dish with id " + dishId + " was successfully deleted.";
     }
 
+    /***
+     * Handles InstanceNotFoundException
+     * @param infe The exception to handle
+     * @return Status Code 404 and error message
+     */
     @ExceptionHandler(InstanceNotFoundException.class)
     public ResponseEntity<String> instanceNotFoundException(InstanceNotFoundException infe) {
         return ResponseEntity.status(404).body(infe.getMessage());
     }
 
+    /***
+     * Handles InstanceAlreadyExistsException
+     * @param iaee The exception to handle
+     * @return Status Code 404 and error message
+     */
     @ExceptionHandler(InstanceAlreadyExistsException.class)
     public ResponseEntity<String> instanceAlreadyExistException(InstanceAlreadyExistsException iaee) {
         return ResponseEntity.status(404).body(iaee.getMessage());
