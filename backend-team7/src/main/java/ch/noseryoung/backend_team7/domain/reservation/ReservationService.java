515743fee503b476dbb87d850ea817e29a1fb99d
MaximilianNoethe <maximilian.noethe@noseryoung.com>
2024-06-25 15:55:03 +0200

    Add validateReservation method

--- backend-team7/src/main/java/ch/noseryoung/backend_team7/domain/reservation/ReservationService.java
+++ backend-team7/src/main/java/ch/noseryoung/backend_team7/domain/reservation/ReservationService.java
@@ -1,16 +1,22 @@
 package ch.noseryoung.backend_team7.domain.reservation;
 
+import ch.noseryoung.backend_team7.domain.table.RestaurantTable;
+import ch.noseryoung.backend_team7.domain.table.RestaurantTableRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import javax.management.InstanceAlreadyExistsException;
 import javax.management.InstanceNotFoundException;
+import java.time.LocalTime;
 import java.util.List;
 
 @Service
 public class ReservationService {
     @Autowired
     private ReservationRepository reservationRepository;
+
+    @Autowired
+    private RestaurantTableRepository restaurantTableRepository;
 
     public List<Reservation> getAllReservations() {
         return reservationRepository.findAll();
@@ -24,7 +30,7 @@ public class ReservationService {
      * When selected table is already reserved, exception should be thrown.
      * If reservation time is too early or too late, exception should be thrown.
      **/
-    public Reservation addReservation(Reservation newReservation) {
+    public Reservation addReservation(Reservation newReservation) throws TableAlreadyReservedException, InvalidReservationTimeException {
         return reservationRepository.save(newReservation);
     }
 
@@ -46,5 +52,22 @@ public class ReservationService {
             throw new InstanceNotFoundException("Reservation with id " + reservationId + " could not be found.");
         }
         reservationRepository.deleteById(reservationId);
+    }
+
+
+    private void validateReservation(Reservation reservation) throws TableAlreadyReservedException, InvalidReservationTimeException, InstanceNotFoundException {
+        if (reservation.getRestaurantTable() != null) {
+            int tableId = reservation.getRestaurantTable().getTableId();
+            RestaurantTable table = restaurantTableRepository.findById(tableId).orElseThrow(() -> new InstanceNotFoundException("Table with id " + tableId + " could not be found."));
+
+            if (!table.isAvailable()) {
+                throw new TableAlreadyReservedException("Table with id " + tableId + " is already reserved.");
+            }
+        }
+
+        LocalTime reservationTime = reservation.getReservationTime().toLocalTime();
+        if (reservationTime.isBefore(LocalTime.of(12, 0)) || reservationTime.isAfter(LocalTime.of(23, 0))) {
+            throw new InvalidReservationTimeException("Reservation time " + reservationTime + " is outside of the allowed hours (12:00 - 23:00).");
+        }
     }
 }
